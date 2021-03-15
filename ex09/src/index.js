a3aa82a5dc7a5ce27a6d1ec1c42d98d56f73707b
AmarMusic91 <music.amar@hotmail.com>
2021-03-15 23:43:25 +0100

    true false

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,9 +1,9 @@
 function isLess(a, b) {
-    if (a < b) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(isLess(10, 15));
 console.log(isLess(15, 10));
