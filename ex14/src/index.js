975106aa4fcd7b1ac99708001953a20b14d02cd8
AmarMusic91 <music.amar@hotmail.com>
2021-03-16 02:18:56 +0100

    ..

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,7 +1,6 @@
 function convertStrToInt(str) {
-    return parseInt(str);
+  return parseInt(str);
 }
-convertStrToInt();
 console.log(convertStrToInt("44"));
 console.log(convertStrToInt("27"));
 console.log(convertStrToInt("JeffBezos"));
